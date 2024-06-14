d7883d14b73e10227e20eed3c1a518e9decfe462
Chris Abdo <chris.j.abdo@gmail.com>
2024-06-13 22:33:39 -0400

    fin

--- Main.java
+++ Main.java
@@ -6,14 +6,6 @@
 // ex: 6 is 3 spaces to the right of 3,
 // ask user for number input
 // find all prime numbers up to that number
-// sieve of eratosthenes
-// for loop. if statement. if 0 skip it. if not 0, actually go through the array and knock out the multiples of that number
-// sqrt. if. else.
-
-// goldbach's conjecture
-// for loop to go from 4 to <= n ex. 1 million
-// search the array for the first candidate prime value
-// if statement
 
 // example main
 // do sieve
@@ -28,6 +20,7 @@
 //   for()
 //     if()
 // make sure to not use multiplication, division, or modulo for finding the prime numbers
+// this was a tricky one. i had to look up the sieve of eratosthenes and goldbach's conjecture to learn a little more
 
 import java.util.Scanner;
 
@@ -35,57 +28,74 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print(
-            "Enter the number up to which you want to find primes: "
-        );
+        System.out.print("Number to find prime numbers up to (min 2): ");
         int num = scanner.nextInt();
 
-        // 0 and 1 not prime
+        // doesnt work for 0 or 1
         if (num < 2) {
-            System.out.println("Minimum # allowed is 2.");
+            System.out.println("Min # allowed is 2.");
             return;
         }
-        int[] arr = new int[num + 1];
 
-        sieve(arr);
+        // num + 1 to account for 0 index
+        int[] array = new int[num + 1];
 
-        System.out.println("Prime numbers up to " + num + ":");
+        sieve(array);
+
+        System.out.println("Sieve of Eratosthenes for 2 to " + num + ": ");
         for (int i = 1; i <= num; i++) {
-            if (arr[i] != 0) {
+            // if val at index i is not 0 print it
+            if (array[i] != 0) {
                 System.out.print(i + " ");
             }
         }
 
-        goldbach(arr);
+        System.out.println();
+        System.out.println("Goldbach's Conjecture: ");
+        goldbach(array);
+
         scanner.close();
     }
 
+    // sieve of eratosthenes
+    // for loop. if statement. if 0 skip it. if not 0, actually go through the array and knock out the multiples of that number
+    // sqrt. if. else.
     public static void sieve(int[] array) {
-        int n = array.length - 1;
+        // - 1 to account for 0 index
+        int nums = array.length - 1;
 
-        // init array and mark all vals as prime (for now)
-        for (int i = 1; i <= n; i++) {
+        // init array and assume all prime
+        for (int i = 1; i <= nums; i++) {
             array[i] = i;
         }
 
-        // Mark non-primes as zero
+        // set 2nd index to 0
         array[1] = 0;
-        for (int i = 2; i * i <= n; i++) {
+
+        // i^2 <= nums
+        // i <= sqrt(nums)
+        for (int i = 2; i * i <= nums; i++) {
             if (array[i] != 0) {
-                for (int j = i * i; j <= n; j += i) {
-                    array[j] = 0;
+                for (int key = i * i; key <= nums; key += i) {
+                    array[key] = 0;
                 }
             }
         }
     }
 
+    // goldbach's conjecture
+    // for loop to go from 4 to <= n ex. 1 million
+    // search the array for the first candidate prime value
     public static void goldbach(int[] array) {
-        int n = array.length - 1;
+        int nums = array.length - 1;
 
-        for (int even = 4; even <= n; even += 2) {
-            for (int i = 2; i <= n; i++) {
-                if (array[i] != 0 && array[even - i] != 0) {
-                    System.out.println(even + " = " + i + " + " + (even - i));
+        // target evens +=2
+        for (int start = 4; start <= nums; start += 2) {
+            // iterate through arr to check if diff is prime
+            for (int i = 2; i <= nums; i++) {
+                if (array[i] != 0 && array[start - i] != 0) {
+                    System.out.println(start + " = " + (start - i) + " + " + i);
+                    // out of bounds error
                     break;
                 }
             }
